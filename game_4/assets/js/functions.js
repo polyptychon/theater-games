eb9d105e05b5212ff06d1bad2c6a178481492c61
George Triantafyllakos <gtrianta@gmail.com>
2015-10-10 00:47:32 +0300

    Changed drag and drop mechanism so that figures can be dropped anywhere on the vas and that snapping only happens when they are dropped on their final position + Changed figures tooltip so that it doesn't interfere with the figure and the dragging + removed scaling when dragging figure

--- game_4/assets/js/functions.js
+++ game_4/assets/js/functions.js
@@ -2,7 +2,7 @@
 var isMobile = false; if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
 var lang = ((getUrlParam("lang") == "gr" || !getUrlParam("lang")) ? "gr" : "en");
 var game_data = "";
-var total_seconds = 60;
+var total_seconds = 30;
 var timer_interval = null;
 var current_level = 0;
 var game_is_running = false;
@@ -18,8 +18,9 @@ $(window).load(function() {
 	$("#help").click(function() { show_help(); });
 	$("#close_help").click(function(event) { hide_help(); event.stopPropagation(); });
 	$("#close_message").click(function(event) { hide_message("down"); event.stopPropagation(); });	
-	$(".figure").draggable({revert: true, start: function() { $(this).css({"z-index":4000}); $(this).data("parent_id",$(this).parent().attr("id")); }, stop: function() { $(this).css({"z-index":200}); if ($(this).parent().attr("id").indexOf("_figures") != -1) remove_figure($(this).attr("id")); } }).click(function() { remove_figure($(this).attr("id")); });
+	$(".figure").draggable({revert: false, start: function() { $(this).css({"z-index":4000}); $(this).data("parent_id",$(this).parent().attr("id")); }, stop: function() { $(this).css({"z-index":200}); } }).click(function() { remove_figure($(this).attr("id")); });/*.mouseover(function() { $(this).attr("theTitle",$(this).find(".speech").html()); $("*[theTitle]").titlesBehaviour(); }); $("#speech").html("").html($(this).find(".speech").html()).addClass("opened").css({top:e.clientX, left:e.clientY}); }).mouseout(function() { $("#speech").html("").removeClass("opened"); }); */
 	$(".position").droppable({drop: function(event,ui) { var position_id = $(this).attr("id"); drop_figure(event,$(ui.draggable).attr("id"),position_id); } });
+	$(".vas, .figures").droppable({drop: function(event,ui) { if ($(this).hasClass("figures")) remove_figure($(ui.draggable).attr("id")); }});
 	$(".vas_check_button").each(function() { $(this).click(function() { check_positions(); }); });
 	$(".vas_hint").each(function() { $(this).mousedown(function() { show_hint(); }).mouseup(function() { hide_hint(); }); });
 	$(document).keyup(function(e) {
@@ -53,6 +54,7 @@ function goto_screen(which) {
 		current_level = which.split("_")[1];
 		position_figures();
 		add_figure_text();
+		$("*[theTitle]").titlesBehaviour();
 		$("#level_" + current_level + " .vas .empty_vas, #level_" + current_level + " .vas .position, #level_" + current_level + "_vas_hint").removeClass("invisible");
 		hide_help();
 		reset_timer(); start_timer();
@@ -71,11 +73,11 @@ function show_message(params) {
 }
 function hide_message(where) { if (where == "down") { if (!$("#popup").hasClass("down")) $("#popup").addClass("down"); else $("#popup").removeClass("down"); } else $("#popup").addClass("invisible"); }
 
-function start_timer() { timer_interval = setInterval(clock_timer,1000); game_is_running = true; }
-function clock_timer() { total_seconds--; var minutes = parseInt(total_seconds/60)%60; var seconds = total_seconds%60; $("#clock").html((minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds)); if (total_seconds == 0) { stop_timer(); check_positions(current_level); } }
+function start_timer() { timer_interval = setInterval(clock_timer,1000); game_is_running = true; $(".figure").draggable("enable"); }
+function clock_timer() { total_seconds--; var minutes = parseInt(total_seconds/60)%60; var seconds = total_seconds%60; $("#clock").html((minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds)); if (total_seconds < 11) $("#clock").addClass("last_ten"); else $("#clock").removeClass("last_ten"); if (total_seconds == 0) { stop_timer(); check_positions(current_level); } }
 function pause_timer() { clearInterval(timer_interval); }
-function stop_timer() { clearInterval(timer_interval); timer_interval = null; total_seconds = 0; }
-function reset_timer() { total_seconds = 60; $("#clock").html("01:00"); }
+function stop_timer() { clearInterval(timer_interval); timer_interval = null; total_seconds = 0; $(".figure").draggable("disable"); }
+function reset_timer() { total_seconds = 30; $("#clock").html("00:30").removeClass("last_ten"); }
 
 function show_hint() { $("#level_" + current_level + " .vas .empty_vas, #level_" + current_level + " .vas .position").addClass("invisible"); total_seconds -= 5; }
 function hide_hint() { $("#level_" + current_level + " .vas .empty_vas, #level_" + current_level + " .vas .position").removeClass("invisible"); }
@@ -92,29 +94,33 @@ function position_figures() {
 function add_figure_text(){
 	$("#level_" + current_level + " .figure").each(function() {
 		var f_id = $(this).attr("id").split("_")[2] + "_" + $(this).attr("id").split("_")[3]; var f_speech = "";
-		$.each(game_data.vases, function(i,v) { if (v.level == current_level) { $.each(v.figures, function(i,w) { if (w.id == f_id) { if (eval("w.text." + lang) != "-") f_speech = eval("w.text." + lang); } }); } });
-		if (f_speech != "") $(this).append("<div class='speech'>" + f_speech + "</div>");
+		$.each(game_data.vases, function(i,v) { if (v.level == current_level) { $.each(v.figures, function(i,w) { if (w.id == f_id) { f_speech = eval("w.text." + lang); } }); } });
+		if (f_speech !== "-") $(this).attr("theTitle", f_speech); else $(this).removeAttr("theTitle");
 	});
 }
 function drop_figure(e,figure_id,position_id) {
 	e.preventDefault();
-	if ($("#" + figure_id).data().parent_id == "level_" + current_level + "_figures") {
-		if ($("#" + position_id).find($(".figure")).length == 0) {
+	$("#" + figure_id).mouseover(function() { hideddrivetip(); });
+	if (position_id == "level_" + current_level + "_figures") remove_figure(figure_id);
+	else {
+		if ($("#" + figure_id).data().parent_id == "level_" + current_level + "_figures") {
+			if ($("#" + position_id).find($(".figure")).length == 0 && position_id.split("_")[3] == figure_id.split("_")[3]) {
+				$("#" + position_id).append($("#" + figure_id));
+				/* $("#" + figure_id + " .speech").addClass("opened").delay(3000).queue(function() { $(this).removeClass("opened").dequeue(); }); */
+			} /* else remove_figure(figure_id); */
+		} else {
+			$("#" + $("#" + figure_id).data().parent_id).append($("#" + position_id + " .figure"));
 			$("#" + position_id).append($("#" + figure_id));
-			$("#" + figure_id + " .speech").addClass("opened").delay(3000).queue(function() { $(this).removeClass("opened").dequeue(); });
-		} else remove_figure(figure_id);
-	} else {
-		$("#" + $("#" + figure_id).data().parent_id).append($("#" + position_id + " .figure"));
-		$("#" + position_id).append($("#" + figure_id));
-		$("#" + figure_id + " .speech").addClass("opened").delay(3000).queue(function() { $(this).removeClass("opened").dequeue(); });
+			/* $("#" + figure_id + " .speech").addClass("opened").delay(3000).queue(function() { $(this).removeClass("opened").dequeue(); }); */
+		}
+		toggle_check_button();
 	}
-	toggle_check_button();
 }
 function toggle_check_button() {
 	if ($("#level_" + current_level + " .position .figure").length == $("#level_" + current_level + " .position").length) $("#level_" + current_level + "_vas_check_button").removeClass("invisible");
 	else $("#level_" + current_level + "_vas_check_button").addClass("invisible");
 }
-function remove_figure(figure_id) { $("#level_" + current_level + " .figures").append($("#" + figure_id)).queue(function() { $("#" + figure_id).css({ "position":"absolute", "top":$("#" + figure_id).attr("top"), "left":$("#" + figure_id).attr("left") }).removeClass("invisible"); toggle_check_button(current_level); $(this).dequeue(); }); }
+function remove_figure(figure_id) { if (game_is_running) { $("#level_" + current_level + " .figures").append($("#" + figure_id)).queue(function() { $("#" + figure_id).css({ "position":"absolute", "top":$("#" + figure_id).attr("top"), "left":$("#" + figure_id).attr("left") }).removeClass("invisible").mouseover(function() { if (typeof $(this).attr("theTitle") !== typeof undefined && $(this).attr("theTitle") !== false) ddrivetip($(this).attr("theTitle")); }); toggle_check_button(current_level); $(this).dequeue(); }); } }
 function check_positions() {
 	// if ($("#level_" + level + " .figures .figure").length == 0) end_game(level);
 	var correct_positions = 0;
@@ -154,7 +160,7 @@ function end_game(level,correct) {
 /* generic and external functions */
 $.fn.extend({ 
 	titlesBehaviour: function() {
-		$(this).each(function() { $(this).mouseover(function() { ddrivetip($(this).attr("theTitle")); }); $(this).mouseout(function() { hideddrivetip(); }); });
+		$(this).each(function() { if ($(this).parent().hasClass("figures")) { $(this).mouseover(function() { ddrivetip($(this).attr("theTitle")); }).mouseout(function() { hideddrivetip(); }).mousedown(function() { hideddrivetip(); }); } });
 	},
 	disableSelection: function() { this.each(function() { this.onselectstart = function() { return false; }; this.unselectable = "on"; $(this).css('-moz-user-select', 'none'); $(this).css('-webkit-user-select', 'none'); }); return this; }
 });
