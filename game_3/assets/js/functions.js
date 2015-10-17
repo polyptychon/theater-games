c0e4a2c294188cb32ea5e68d7e703971ed452d41
George Triantafyllakos <gtrianta@gmail.com>
2015-10-17 21:59:00 +0300

    Minor corrections (mostly for mobile interaction)

--- game_3/assets/js/functions.js
+++ game_3/assets/js/functions.js
@@ -1,6 +1,7 @@
 /* global vars */
+var isIE = $.browser.msie; var isIE8 = $.browser.msie && ($.browser.version.indexOf(8) != -1); var isIE9 = $.browser.msie && ($.browser.version.indexOf(9) != -1);
 var isMobile = false; if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
-var touch = window.ontouchstart || navigator.MaxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+var touch = ('ontouchstart' in document.documentElement);
 var lang = ((getUrlParam("lang") == "gr" || !getUrlParam("lang")) ? "gr" : "en");
 var game_data = "";
 var carousel_interval = "";
@@ -13,22 +14,26 @@ var levels_played = new Array();
 
 /* window/document states */
 $(window).load(function() {
-	init();	
-	$("#close_descriptions").click(function() { $("#descriptions").removeClass("open"); $(".theater").removeClass("selected"); });
-	$("#check_schedule").unbind("click");
-	$("#num_of_theaters").html(0);
-	$("#start_greece").click(function() { goto_screen("level_greece"); });
-	$("#start_mediterranean").click(function() { goto_screen("level_mediterranean"); });
-	$("#enter_game").click(function() { $("#intro").addClass("instructions"); });
-	$("#help").click(function() { show_help(); });
-	$("#close_help").click(function(event) { hide_help(); event.stopPropagation(); });
-	$("#close_message").click(function(event) { hide_message("down"); event.stopPropagation(); });
-	$(document).keyup(function(e) {
-	  if (e.keyCode == 27 /* escape */) { $(".theater").removeClass("selected"); $("#descriptions").removeClass("open"); hide_help(); }
-	  else if (e.keyCode == 13 /* enter */) { $("#theater_button .button").click(); }
-	});
-	disableHover();
-	setTimeout(function() { $("#loader").fadeOut(500, function() { $(this).remove(); }); }, 500);			
+	if (isIE8 || isIE9) {
+		$("body").html("<div style='width:1024px; text-align:center; margin:25px auto;'>This site uses web technologies that your browser (Internet Explorer " + $.browser.version + ") doesn't support.<br/>Please visit the site using a new version of <a href='https://www.google.com/intl/en/chrome/browser/'>Google Chrome</a>, <a href='http://www.mozilla.org/en-US/firefox/new/'>Mozilla Firefox</a>, <a href='https://www.apple.com/safari/'>Safari</a> or <a href='http://www.opera.com/'>Opera</a>.<br/>Sorry for the inconvenience :-/</div>");
+	} else {
+		init();	
+		$("#close_descriptions").click(function() { $("#descriptions").removeClass("open"); $(".theater").removeClass("selected"); });
+		$("#check_schedule").unbind("click");
+		$("#num_of_theaters").html(0);
+		$("#start_greece").click(function() { goto_screen("level_greece"); });
+		$("#start_mediterranean").click(function() { goto_screen("level_mediterranean"); });
+		$("#enter_game").click(function() { $("#intro").addClass("instructions"); });
+		$("#help").click(function() { show_help(); });
+		$("#close_help").click(function(event) { hide_help(); event.stopPropagation(); });
+		$("#close_message").click(function(event) { hide_message("down"); event.stopPropagation(); });
+		$(document).keyup(function(e) {
+		  if (e.keyCode == 27 /* escape */) { $(".theater").removeClass("selected"); $("#descriptions").removeClass("open"); hide_help(); }
+		  else if (e.keyCode == 13 /* enter */) { $("#theater_button .button").click(); }
+		});
+		if (touch) { disableHover(); window.scrollTo(0,1); }
+		setTimeout(function() { $("#loader").fadeOut(500, function() { $(this).remove(); }); }, 500);
+	}
 });
 /* */
 
